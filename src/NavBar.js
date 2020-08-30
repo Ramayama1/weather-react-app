40aadfffae1b78fa10e4accb2867fca4ef5056fd
Mallory Eastburn <mallorykaitlin@gmail.com>
2020-08-30 17:09:08 -0500

    Adjust responsiveness

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,11 +3,11 @@ import "./NavBar.css";
 export default function NavBar() {
   return (
     <nav className="NavBar navbar navbar-dark row">
-      <div className="col">
+      <div className="col center-block text-center">
         <h1 className="navbar-brand">Weather App</h1>
       </div>
-      <div className="col">
-        <form className="form-inline row">
+      <div className="col center-block text-center">
+        <form className="form-inline row-xs center-block text-center">
           <input
             className="form-control mr-sm-2 searchCityID"
             type="search"
